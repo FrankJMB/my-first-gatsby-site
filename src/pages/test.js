85644d0099f26953d2ca2b71bf4d9d1d04556127
Frank <frank.beentjes@live.nl>
2023-10-10 12:47:27 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/test.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const TestPage = () => {
+  return (
+    <main>
+      <h1>Test Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>Test Me</title>
+
+export default TestPage
